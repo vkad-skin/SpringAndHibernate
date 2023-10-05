3c93c288348dc5bf947b34be77b8e914900b636b
Vladislav <vladikul98@gmail.com>
2023-10-05 18:52:32 +0300

    2.4 version (use deleteMapping)

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -3,10 +3,7 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
@@ -46,7 +43,7 @@ public class UsersController {
         return "info-user";
     }
 
-    @GetMapping("/delete-user")
+    @DeleteMapping("/delete-user")
     public String deleteUser(@RequestParam("userId") int id) {
         userService.deleteUserById(id);
         return "redirect:/show-all-users";
